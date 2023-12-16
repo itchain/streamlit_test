7e5352f4b738b8db9bb4dbd96e726d40f2cc3586
Peter Kim <itchain@gmail.com>
2023-12-16 13:01:46 +0900

    new file

---
+++ pages/About.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# st.title('Uber pickups in NYC')
+
+st.text('이 곳은 하위페이지 입니다')
+st.subheader('Number of pickups by hour')
---
+++ pages/한글.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.text('이 곳은 하위페이지 입니다')
+
+st.text("Hello World")
+st.code('''## 안녕 **하세요**
+긴문장도 표시가 되나요?
+''')
