a74ced3b9ec6e81b4dd848fc9c749953a074f507
Peter Kim <itchain@gmail.com>
2023-12-17 13:50:07 +0900

    add chatgpt-like chat

---
+++ pages/ChatGPT.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from openai import OpenAI
+
+st.title("ChatGPT-like clone")
+
+OPENAI_API_KEY = "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"
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+    
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
