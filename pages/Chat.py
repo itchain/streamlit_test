c769549f06d5eca41621a3a27916d9a6ade4bf66
Peter Kim <itchain@gmail.com>
2023-12-17 12:28:30 +0900

    simple chat

--- pages/Chat.py
+++ pages/Chat.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import streamlit as st
-import streamlit as st
+import random
+import time
 
-st.title("Echo Bot")
+st.title("Simple Chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -18,3 +18,20 @@ if prompt := st.chat_input("무슨 생각을 하고 있나요?"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Hi, human! Is there anything I can help you with?",
+                "Do you need help?",
+            ]
+        )
+
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
